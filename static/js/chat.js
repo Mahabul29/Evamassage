cb323ddf0d85e8ee22b462fa81c6655b78d8b003
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-06-07 15:51:02 +0530

    Update chat.js

--- static/js/chat.js
+++ static/js/chat.js
@@ -57,3 +57,50 @@ class ChatManager {
     
     sendMessage() {
         const message = $('#messageInput').val();
+        if (message && this.currentChat) {
+            $.post('/api/messages/send', {
+                to_id: this.currentChat,
+                message: message
+            }, () => {
+                $('#messageInput').val('');
+                this.loadMessages();
+            });
+        }
+    }
+    
+    sendTypingIndicator() {
+        clearTimeout(this.typingTimeout);
+        $.post('/api/typing', { to_id: this.currentChat });
+        this.typingTimeout = setTimeout(() => {
+            $.post('/api/stop_typing', { to_id: this.currentChat });
+        }, 2000);
+    }
+    
+    markAsRead() {
+        if (this.currentChat) {
+            $.post('/api/messages/mark_read', { user_id: this.currentChat });
+        }
+    }
+    
+    startPolling() {
+        this.messageInterval = setInterval(() => {
+            if (this.currentChat) {
+                this.loadMessages();
+            }
+        }, 3000);
+    }
+    
+    scrollToBottom() {
+        const area = document.getElementById('messagesArea');
+        if (area) area.scrollTop = area.scrollHeight;
+    }
+    
+    escapeHtml(text) {
+        const div = document.createElement('div');
+        div.textContent = text;
+        return div.innerHTML;
+    }
+}
+
+// Initialize chat
+const chat = new ChatManager();
