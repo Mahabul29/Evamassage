2a9a3c8c90a032f6c0574f730acb5ceb036039d7
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-06-08 00:34:41 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,19 +1,10 @@
 const CACHE_NAME = 'evamassage-v1';
-const urlsToCache = [
-  '/',
-  '/static/manifest.json'
-];
+const urlsToCache = ['/', '/static/manifest.json', '/static/css/style.css', '/static/js/app.js', '/static/js/chat.js', '/static/js/channel.js', '/static/js/install.js'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
