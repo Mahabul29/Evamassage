709380115d0aed75f50f3915cb2c195171f48b2d
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-06-08 00:22:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from pymongo import MongoClient
 
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/')
-MONGO_DB_NAME = os.environ.get('MONGO_DB_NAME', 'evamassage')
-DATABASE_TYPE = os.environ.get('DATABASE_TYPE', 'mongodb')
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-change-this')
+DB_NAME = os.environ.get('DB_NAME', 'evamassage')
+SECRET_KEY = 'your-secret-key-12345'
 PORT = int(os.environ.get('PORT', 8080))
-FQDN = os.environ.get('FQDN', 'http://localhost:8080')
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+
+# Database connection
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+# Collections
+users = db['users']
+messages = db['messages']
+chats = db['chats']
+channels = db['channels']
+channel_members = db['channel_members']
+channel_msgs = db['channel_messages']
